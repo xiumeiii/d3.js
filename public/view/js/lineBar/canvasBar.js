b98209996b58d26170eec55eebb8310f6d2897d9
xiumeiii <779048883@qq.com>
2016-09-07 18:18:16 +0800

    绘制柱状图

--- public/view/js/lineBar/canvasBar.js
+++ public/view/js/lineBar/canvasBar.js
@@ -331,5 +331,55 @@
 
 //d3-v3 barChartIIIc
 $(function(){
-
+    var margin = {top: 20, right: 30, bottom: 30, left: 40},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+    var x = d3.scale.ordinal()
+        .rangeRoundBands([0,width],.1);
+    var y = d3.scale.linear()
+        .range([height,0]);
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+    var chart = d3.select("#barChartIIIc").append("svg").attr("class","chart")
+        .attr("width",width + margin.left + margin.right)
+        .attr("height",height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform","translate(" + margin.left + "," + margin.top + ")");
+    d3.tsv("./../data/lineBar/barChartIIIa.tsv",type,function(error,data){
+        x.domain(data.map(function(d){
+            return d.name;
+        }));
+        y.domain([0,d3.max(data,function(d){
+            return d.value;
+        })]);
+        chart.append("g")
+            .attr("class","x axis")
+            .attr("transform","translate(0" + height + ")")
+            .call(xAxis);
+        chart.append("g")
+            .attr("class","y axis")
+            .attr(yAxis);
+        chart.selectAll(".bar")
+            .data(data)
+            .enter().append("rect")
+            .attr("class","bar")
+            .attr("x",function(d){
+                return x(d.name);
+            })
+            .attr("y",function(d){
+                return y(d.value);
+            })
+            .attr("height",function(d){
+                return height - y(d.value);
+            })
+            .attr("width", x.rangeBand());
+    });
+    function type(d){
+        d.value = +d.value;
+        return d;
+    }
 })
