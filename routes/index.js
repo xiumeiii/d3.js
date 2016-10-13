1cbb332916c61431093048597a9d04c9cdbf71b0
xiumeiii <779048883@qq.com>
2016-10-13 17:36:20 +0800

    绘制柱状图

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'd3Demo' });
 });
 
+//router.get('/getUser', function(req, res, next) {
+//  res.send({
+//    'user':'aaa'
+//  })
+//});
+
+
 module.exports = router;
